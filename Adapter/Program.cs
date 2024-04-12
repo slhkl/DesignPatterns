54dab50fa353021205a678b684ce2bb06f8d8969
Salih Kol <salih.kol@detaysoft.com>
2024-04-12 14:08:33 +0300

    Add project files.

---
+++ Adapter/Program.cs
@@ -0,0 +1,29 @@
+﻿Target target = new Adapter(new Adaptee());
+target.Request();
+
+public class Adapter : Target
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public override void Request()
+    {
+        _adaptee.SpecificRequest();
+    }
+}
+public class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Console.WriteLine("Called SpecificRequest()");
+    }
+}
+
+public abstract class Target
+{
+    public abstract void Request();
+}
